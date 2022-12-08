9953ce567132e1dcff5e0098c929a5a9d1302371
Emile Villette <emile.villette@student.uclouvain.be>
2022-12-08 22:25:15 +0100

    added files

--- src/main/java/graphs/ConnectedComponents.java
+++ src/main/java/graphs/ConnectedComponents.java
@@ -16,8 +16,6 @@ import java.util.List;
  * }
  * Hint: Feel free to add methods or even inner-class (private static class) to help you but don't change the class name or the signature of the numberOfConnectedComponents method.
  * Don't forget to add the imports at the beginning of your code if you use objects from the Java API.
- *
- *
  */
 public class ConnectedComponents {
 
@@ -27,18 +25,57 @@ public class ConnectedComponents {
      */
     public static int numberOfConnectedComponents(Graph g) {
         // TODO
-         return -1;
+        UF uf = new UF(g.V());
+        for(int i =0;i<g.V();i++) {
+            for(int j: g.adj(i)) {
+                uf.union(i,j);
+            }
+        }
+        return uf.count;
+    }
+
+    private static class UF {
+        private int[] id; // access to component id (site indexed)
+        private int count; // number of components
+
+        public UF(int N) { // Initialize component id array.
+            count = N;
+            id = new int[N];
+            for (int i = 0; i < N; i++)
+                id[i] = i;
+        }
+
+        public int count() {
+            return count;
+        }
+
+        public boolean connected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        private int find(int p) { // Find component name.
+            while (p != id[p]) p = id[p];
+            return p;
+        }
+
+        public void union(int p, int q) { // Give p and q the same root.
+            int pRoot = find(p);
+            int qRoot = find(q);
+            if (pRoot == qRoot) return;
+            id[pRoot] = qRoot;
+            count--;
+        }
+
+        // See page 222 (quick-find),page 224 (quick-union) andpage 228 (weighted).
     }
 
     static class Graph {
 
         private List<Integer>[] edges;
 
-        public Graph(int nbNodes)
-        {
+        public Graph(int nbNodes) {
             this.edges = (ArrayList<Integer>[]) new ArrayList[nbNodes];
-            for (int i = 0;i < edges.length;i++)
-            {
+            for (int i = 0; i < edges.length; i++) {
                 edges[i] = new ArrayList<>();
             }
         }
@@ -59,7 +96,7 @@ public class ConnectedComponents {
                 count += bag.size();
             }
 
-            return count/2;
+            return count / 2;
         }
 
         /**
